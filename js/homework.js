580145565a34ab7340e72bbf41cc002fb1e188f8
Ann <a.s.makarovaa@gmail.com>
2021-03-29 18:34:05 +0500

    Сумма и определение года

---
+++ js/homework.js
@@ -0,0 +1,118 @@
+'use strict';
+ 
+/**
+ * Складывает два целых числа
+ * @param {Number} a Первое целое
+ * @param {Number} b Второе целое
+ * @throws {TypeError} Когда в аргументы переданы не числа
+ * @returns {Number} Сумма аргументов
+ */
+function abProblem(a, b) {
+    // Ваше решение
+    let c = a + b
+    if (typeof a !== 'number') {
+        throw new Error('this is not a number');
+    }else if (typeof b !== 'number') {
+        throw new Error('this is not a number');
+    }else
+        console.log(c)
+}
+
+abProblem('hehe', 20)
+ 
+/**
+ * Определяет век по году
+ * @param {Number} year Год, целое положительное число
+ * @throws {TypeError} Когда в качестве года передано не число
+ * @throws {RangeError} Когда год – отрицательное значение
+ * @returns {Number} Век, полученный из года
+ */
+
+// ДОБАВИТЬ ПРАВИЛЬНУЮ ОБРАБОТКУ ИСКЛЮЧЕНИЙ
+function centuryByYearProblem(year) {
+    // Ваше решение
+    if (typeof year !== 'number') {
+        throw new Error('это не число');
+    } else if (year <=0) {
+        throw new Error('значение не может быть отрицательным');
+    }
+   let v = Math.floor((year-1)/100) + 1;
+   console.log(v);
+}
+centuryByYearProblem(2001)
+
+
+/**
+ * Переводит цвет из формата HEX в формат RGB
+ * @param {String} hexColor Цвет в формате HEX, например, '#FFFFFF'
+ * @throws {TypeError} Когда цвет передан не строкой
+ * @throws {RangeError} Когда значения цвета выходят за пределы допустимых
+ * @returns {String} Цвет в формате RGB, например, '(255, 255, 255)'
+ */
+function colorsProblem(hexColor) {
+    // Ваше решение
+}
+ 
+/**
+ * Находит n-ое число Фибоначчи
+ * @param {Number} n Положение числа в ряде Фибоначчи
+ * @throws {TypeError} Когда в качестве положения в ряде передано не число
+ * @throws {RangeError} Когда положение в ряде не является целым положительным числом
+ * @returns {Number} Число Фибоначчи, находящееся на n-ой позиции
+ */
+function fibonacciProblem(n) {
+    // Ваше решение
+}
+ 
+/**
+ * Транспонирует матрицу
+ * @param {(Any[])[]} matrix Матрица размерности MxN
+ * @throws {TypeError} Когда в функцию передаётся не двумерный массив
+ * @returns {(Any[])[]} Транспонированная матрица размера NxM
+ */
+function matrixProblem(matrix) {
+    // Ваше решение
+    throw new TypeError("somethign wrong");
+}
+ 
+/**
+ * Переводит число в другую систему счисления
+ * @param {Number} n Число для перевода в другую систему счисления
+ * @param {Number} targetNs Система счисления, в которую нужно перевести (Число от 2 до 36)
+ * @throws {TypeError} Когда переданы аргументы некорректного типа
+ * @throws {RangeError} Когда система счисления выходит за пределы значений [2, 36]
+ * @returns {String} Число n в системе счисления targetNs
+ */
+function numberSystemProblem(n, targetNs) {
+    // Ваше решение
+}
+ 
+/**
+ * Проверяет соответствие телефонного номера формату
+ * @param {String} phoneNumber Номер телефона в формате '8–800–xxx–xx–xx'
+ * @throws {TypeError} Когда в качестве аргумента передаётся не строка
+ * @returns {Boolean} Если соответствует формату, то true, а иначе false
+ */
+function phoneProblem(phoneNumber) {
+    // Ваше решение
+}
+ 
+/**
+ * Определяет количество улыбающихся смайликов ":-)" в строке
+ * @param {String} text Строка в которой производится поиск
+ * @throws {TypeError} Когда в качестве аргумента передаётся не строка
+ * @returns {Number} Количество улыбающихся смайликов в строке
+ */
+function smilesProblem(text) {
+    // Ваше решение
+}
+ 
+/**
+ * Определяет победителя в игре "Крестики-нолики"
+ * Тестами гарантируются корректные аргументы.
+ * @param {(('x' | 'o')[])[]} field Игровое поле 3x3 завершённой игры
+ * @returns {'x' | 'o' | 'draw'} Результат игры
+ */
+function ticTacToeProblem(field) {
+    // Ваше решение
+}
